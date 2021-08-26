e34a829aafeaf490808dc52ea93f8e56d3ee2269
vlasty41 <vvv125@seznam.cz>
2021-08-26 02:41:31 +0200

    JS hover, added links

--- files/js/scripts.js
+++ files/js/scripts.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
     $("#test").click(function() {
         document.getElementById("test").innerHTML = "JAVASCRIPT ✔️";
     });
+    $(".links").hover(function() {
+        this.style.background = "rgba(147, 255, 96, 0.6)";
+        this.style["box-shadow"] = "0px 0px 18px rgba(147, 255, 96, 0.6)";
+    });
+    $(".links").on( "mouseleave", function() {
+        this.style.background = "gray";
+        this.style["box-shadow"] = "none";
+  })
 })
