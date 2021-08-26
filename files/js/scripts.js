33bcd229f735577623ee0cbb7eb9e45f9455c369
vlasty41 <vvv125@seznam.cz>
2021-08-26 04:14:12 +0200

    Other tweaks

--- files/js/scripts.js
+++ files/js/scripts.js
@@ -4,10 +4,17 @@ $(document).ready(function() {
     });
     $(".links").hover(function() {
         this.style.background = "rgba(147, 255, 96, 0.6)";
-        this.style["box-shadow"] = "0px 0px 18px rgba(147, 255, 96, 0.6)";
+        this.style["box-shadow"] = "0px 0px 20px rgba(147, 255, 96, 0.6)";
     });
     $(".links").on( "mouseleave", function() {
-        this.style.background = "gray";
+        this.style.background = "none";
         this.style["box-shadow"] = "none";
-  })
+        this.style["box-shadow"] = "inset 0 0 20px white";
+    })
+    $("object").hover(function() {
+        this.style["box-shadow"] = "0px 0px 20px rgba(147, 255, 96, 0.6)";
+    });
+    $("object").on( "mouseleave", function() {
+        this.style["box-shadow"] = "none";
+    })
 })
