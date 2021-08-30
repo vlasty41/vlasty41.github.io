505196108a05f8f5bd055507a2725a2ab5d7e28f
vlasty41 <vvv125@seznam.cz>
2021-08-30 04:14:22 +0200

    Uploaded png

--- files/js/scripts.js
+++ files/js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("#test").click(function() {
         document.getElementById("test").innerHTML = "JAVASCRIPT ✔️";
     });
-    
+
     $(".links").hover(function() {
         this.style.background = "rgba(147, 255, 96, 0.6)";
         this.style["box-shadow"] = "0px 0px 20px rgba(147, 255, 96, 0.6)";
@@ -16,6 +16,12 @@ $(document).ready(function() {
         this.style["box-shadow"] = "0px 0px 20px rgba(147, 255, 96, 0.6)";
     });
     $("object").on( "mouseleave", function() {
+        this.style["box-shadow"] = "none";
+    })
+    $("#carousel").hover(function() {
+        this.style["box-shadow"] = "0px 0px 20px rgba(147, 255, 96, 0.6)";
+    });
+    $("#carousel").on( "mouseleave", function() {
         this.style["box-shadow"] = "none";
     })
 })
